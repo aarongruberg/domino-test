1bdcf2f10245379e9aeadc0d052ef4b0add821ca
Aaron Gruberg <aaron.gruberg@dominodatalab.com>
2019-05-06 09:56:06 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-def printstring(val):
+# This script is to be called from a Domino Model API, it will take in some value and return that same value.
+# This will test calling a python script from an imported git repo in Domino via /repos
+def test(val):
   return val
